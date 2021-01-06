755789db98a9e4c76944e4186b96be06bd15b762
fahlepyrizal01 <fahlepyrizal@gmail.com>
2021-01-06 23:15:31 +0700

    fix dark mode

--- app/src/main/java/com/fahlepyrizal01/githubusers/presenter/fragment/SplashFragment.kt
+++ app/src/main/java/com/fahlepyrizal01/githubusers/presenter/fragment/SplashFragment.kt
@@ -1,15 +1,20 @@
 package com.fahlepyrizal01.githubusers.presenter.fragment
 
+import android.content.Context
+import android.content.res.Configuration.UI_MODE_NIGHT_MASK
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.fahlepyrizal01.githubusers.databinding.FragmentSplashBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import android.R.color as androidColorResource
 
 class SplashFragment : Fragment() {
 
@@ -27,6 +32,13 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        context?.isDarkTheme().let {
+            @Suppress("DEPRECATION")
+            binding.tvName.setTextColor(resources.getColor(
+                if (it == true) androidColorResource.white else androidColorResource.black
+            ))
+        }
+
         lifecycleScope.launch {
             delay(THIRD_SECOND)
             super.getView()?.findNavController()?.navigate(
@@ -34,6 +46,10 @@ class SplashFragment : Fragment() {
             )
         }
 
+    }
+
+    private fun Context.isDarkTheme(): Boolean {
+        return resources.configuration.uiMode and UI_MODE_NIGHT_MASK == UI_MODE_NIGHT_YES
     }
 
     companion object {
